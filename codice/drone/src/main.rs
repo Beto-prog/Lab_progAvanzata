ac59d250a4b29327f8fa2400204c34906467f6d0
Mattia Ferretti <mattia.ferretti@studenti.unitn.it>
2024-12-03 11:37:19 +0100

    improved code quality

--- codice/drone/src/main.rs
+++ codice/drone/src/main.rs
@@ -1,6 +1,7 @@
 use crossbeam_channel::{select_biased, Receiver, Sender};
 use rand::{Rng};
 use std::collections::HashMap;
+use rand::rngs::ThreadRng;
 use wg_2024::controller::NodeEvent::{PacketDropped, PacketSent};
 use wg_2024::controller::{DroneCommand, NodeEvent};
 use wg_2024::drone::Drone;
@@ -14,6 +15,7 @@ struct TrustDrone {
     packet_recv: Receiver<Packet>,
     pdr: f32,
     packet_send: HashMap<NodeId, Sender<Packet>>,
+    rng: ThreadRng,
 }
 
 impl Drone for TrustDrone {
@@ -32,29 +34,17 @@ impl Drone for TrustDrone {
             packet_recv,
             packet_send,
             pdr,
+            rng: rand::thread_rng(),
         }
     }
 
     fn run(&mut self) {
-        let mut rng = rand::thread_rng();
         loop {
             select_biased! {
                  recv(self.controller_recv) -> command => {
                     match command {
                         Ok(command) => {
-                            match command {
-                                DroneCommand::AddSender(id,sender) => {
-                                    self.add_sender(id, sender);
-                                },
-                                DroneCommand::SetPacketDropRate(pdr) => {
-                                    self.set_packet_drop_rate(pdr);
-                                },
-                                DroneCommand::Crash => {
-                                    println!("Drone with id {} crashed", self.id);
-                                    break;
-                                }
-                                DroneCommand::RemoveSender(_) => todo!() 
-                            }
+                           self.handle_command(command);
                         },
                         Err(e) => {
                             eprintln!("Error on command reception of drone {}: {}", self.id, e)
@@ -63,46 +53,8 @@ impl Drone for TrustDrone {
                 }
                 recv(self.packet_recv) -> packet => {
                     match packet {
-                        Ok(mut packet) => {
-                            let routing_headers = &mut packet.routing_header;
-                            if routing_headers.hops[routing_headers.hop_index] != self.id {
-                                self.send_nack(routing_headers, NackType::UnexpectedRecipient(self.id));
-                                continue
-                            }
-
-                            routing_headers.hop_index += 1;
-
-                            if routing_headers.hop_index == routing_headers.hops.len() {
-                                self.send_nack(routing_headers, NackType::DestinationIsDrone);
-                                continue
-                            }
-
-                            let next_hop = routing_headers.hops[routing_headers.hop_index];
-
-                            if !self.is_next_hop_neighbour(next_hop) {
-                                self.send_nack(routing_headers, NackType::ErrorInRouting(next_hop));
-                                continue
-                            }
-
-
-                            match packet.pack_type {
-                              PacketType::MsgFragment(_) => {
-                                    let should_drop = rng.gen_range(0.0..1.0) < self.pdr;
-                                    if should_drop {
-                                        self.send_nack(routing_headers, NackType::Dropped);
-                                    } else {
-                                        self.send_packet(next_hop, packet);
-                                    }
-                                },
-                              PacketType::Nack(_) => {
-                                        self.send_packet(next_hop, packet);
-                                },
-                              PacketType::Ack(_) => {
-                                        self.send_packet(next_hop, packet);
-                                },
-                              PacketType::FloodRequest(_) => {},
-                              PacketType::FloodResponse(_) => {}
-                            }
+                        Ok(packet) => {
+                           self.handle_packet(packet);
                         },
                         Err(e) => {
                             eprintln!("Error on packet reception of drone {}: {}", self.id, e)
@@ -115,6 +67,63 @@ impl Drone for TrustDrone {
 }
 
 impl TrustDrone {
+    fn handle_command(&mut self, command: DroneCommand) {
+        match command {
+            DroneCommand::AddSender(id, sender) => {
+                self.add_sender(id, sender);
+            }
+            DroneCommand::SetPacketDropRate(pdr) => {
+                self.set_packet_drop_rate(pdr);
+            }
+            DroneCommand::Crash => {
+                println!("Drone with id {} crashed", self.id);
+                // TODO implement drone crashing
+            }
+            DroneCommand::RemoveSender(_) => todo!()
+        }
+    }
+
+    fn handle_packet(&mut self, mut packet: Packet) {
+        let routing_headers = &mut packet.routing_header;
+        if routing_headers.hops[routing_headers.hop_index] != self.id {
+            self.send_nack(routing_headers, NackType::UnexpectedRecipient(self.id));
+            return;
+        }
+
+        routing_headers.hop_index += 1;
+
+        if routing_headers.hop_index == routing_headers.hops.len() {
+            self.send_nack(routing_headers, NackType::DestinationIsDrone);
+            return;
+        }
+
+        let next_hop = routing_headers.hops[routing_headers.hop_index];
+
+        if !self.is_next_hop_neighbour(next_hop) {
+            self.send_nack(routing_headers, NackType::ErrorInRouting(next_hop));
+            return;
+        }
+
+        match packet.pack_type {
+            PacketType::MsgFragment(_) => {
+                let should_drop = self.rng.gen_range(0.0..1.0) < self.pdr;
+                if should_drop {
+                    self.send_nack(routing_headers, NackType::Dropped);
+                } else {
+                    self.send_valid_packet(next_hop, packet);
+                }
+            }
+            PacketType::Nack(_) => {
+                self.send_valid_packet(next_hop, packet);
+            }
+            PacketType::Ack(_) => {
+                self.send_valid_packet(next_hop, packet);
+            }
+            PacketType::FloodRequest(_) => {}
+            PacketType::FloodResponse(_) => {}
+        }
+    }
+
     fn add_sender(&mut self, id: NodeId, sender: Sender<Packet>) {
         self.packet_send.insert(id, sender);
     }
@@ -134,32 +143,39 @@ impl TrustDrone {
             .expect("Failed to send message to simulation controller");
     }
 
+    fn reverse_headers(source_routing_header: &SourceRoutingHeader) -> SourceRoutingHeader {
+        let mut new_hops = source_routing_header.hops[..source_routing_header.hop_index].to_vec();
+        new_hops.reverse();
+        let new_headers = SourceRoutingHeader {
+            hops: new_hops,
+            hop_index: 1,
+        };
+        new_headers
+    }
+
     fn send_nack(&mut self, routing_headers: &SourceRoutingHeader, nack_type: NackType) {
-        let mut new_headers = routing_headers.hops[..routing_headers.hop_index].to_vec();
-        new_headers.reverse();
+        let new_headers = Self::reverse_headers(routing_headers);
 
         let is_dropped = match &nack_type {
             NackType::Dropped => true,
             _ => false,
         };
 
-        let next_hop = new_headers[1];
+        let next_hop = new_headers.hops[1];
+
         let nack = Packet {
             pack_type: PacketType::Nack(Nack {
                 fragment_index: 0,
                 nack_type,
             }),
-            routing_header: SourceRoutingHeader {
-                hops: new_headers,
-                hop_index: 1,
-            },
+            routing_header: new_headers,
             session_id: 0,
         };
 
         if is_dropped {
             self.drop_packet(next_hop, nack)
         } else {
-            self.send_packet(next_hop, nack);
+            self.send_valid_packet(next_hop, nack);
         }
     }
 
@@ -169,22 +185,18 @@ impl TrustDrone {
         match sender {
             None => {}
             Some(sender) => {
-                sender.send(packet.clone()).expect("Sender should be valid");
-                self.send_packet_sent_event(packet);
+                sender.send(packet).expect("Sender should be valid");
             }
         }
     }
 
+    fn send_valid_packet(&mut self, dest_id: NodeId, packet: Packet) {
+        self.send_packet(dest_id, packet.clone());
+        self.send_packet_sent_event(packet);
+    }
     fn drop_packet(&mut self, dest_id: NodeId, packet: Packet) {
-        let sender = self.packet_send.get(&dest_id);
-
-        match sender {
-            None => {}
-            Some(sender) => {
-                sender.send(packet.clone()).expect("Sender should be valid");
-                self.send_packet_dropped_event(packet);
-            }
-        }
+        self.send_packet(dest_id, packet.clone());
+        self.send_packet_dropped_event(packet);
     }
 
     fn is_next_hop_neighbour(&self, next_hop: NodeId) -> bool {
