8ef0b51198e0854337300c81786cdccb60c9ba8c
Beto <betorov03@gmail.com>
2024-12-08 16:27:39 +0100

    Added networinit

--- codice/network_init/src/main.rs
+++ codice/network_init/src/main.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use serde::Deserialize;
-
+use TrustDrone::TrustDrone as TrsDrone;
+use wg_2024::drone::Drone as DroneTrait;
 #[derive(Debug, Deserialize)]
 struct NetworkConfig {
     drone: Vec<Drone>,
@@ -26,20 +27,51 @@ struct Server {
     id: u8,
     connected_drone_ids: Vec<u8>,
 }
+/*
+fn start_drone <T> ( done : T) where T : DroneTrait
+{
+    thread::spawn(move || {
+        // drone.run();
+    });   
+}*/
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     // Read toml file
     let toml_content = std::fs::read_to_string("network_config.toml")?;
 
-    // Deserializza il contenuto in una struttura Rust
-    let config: NetworkConfig = toml::from_str(&toml_content)?;
-
     
-    // Stampa i dettagli
-    println!("{:#?}", config.client);
-    thread::spawn(move || {
-        drone.run();
-    });
+    // Serialize data 
+    let config: NetworkConfig = toml::from_str(&toml_content)?;
+    
+    
+    for drone in config.drone {
+
+        
+        //spawn drone 
+        thread::spawn(move || {
+            //start_drone (drone);
+            // copy server
+        });
+        
+    }
+    
+    for client in config.client {
+        
+        //spawn client 
+        thread::spawn(move || {
+            // copy drone
+        });
+    }
+    
+    for server in config.server {
+        //spawn server 
+        thread::spawn(move || {
+            // copy server
+        });
+    }
+    
+    
+  
     Ok(())
 }
