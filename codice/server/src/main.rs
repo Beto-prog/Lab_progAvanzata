9c6a637e454e1e3f7e91a973e7959f20bdaa73b0
Beto <betorov03@gmail.com>
2024-12-12 00:13:28 +0100

    Added first functions of the server

--- codice/server/src/main.rs
+++ codice/server/src/main.rs
@@ -1,95 +1,87 @@
-mod message;
-
-use crate::message::{ChatRequest, ChatResponse, DroneSend, Message, Request, Response};
-use wg_2024::network::topology::ServerType;
 use wg_2024::network::*;
+use std::collections::{HashMap, VecDeque};
 
-pub trait Server {
-    type RequestType: Request;
-    type ResponseType: Response;
+pub enum ServerType
+{
+    TextServer,
+    MediaServer,
+}
+trait Server 
+{
 
-    fn compose_message(
-        source_id: NodeId,
-        session_id: u64,
-        raw_content: String,
-    ) -> Result<Message<Self::RequestType>, String> {
-        let content = Self::RequestType::from_string(raw_content)?;
-        Ok(Message {
-            session_id,
-            source_id,
-            content,
-        })
-    }
+    
+    //It gives back the shortest path possible  BFS  complexity O(V+E) , shoutout to Montressor.
+    fn bfs_shortest_path(
+        graph: &HashMap<NodeId, Vec<NodeId>>,
+        start: NodeId,
+        goal: NodeId,
+    ) -> Option<Vec<NodeId>> {
+        let mut queue = VecDeque::new();
+        let mut visited = HashMap::new();
+        let mut parent = HashMap::new();
 
-    fn on_request_arrived(&mut self, source_id: NodeId, session_id: u64, raw_content: String) {
-        if raw_content == "ServerType" {
-            let _server_type = Self::get_sever_type();
-            // send response
-            return;
-        }
-        match Self::compose_message(source_id, session_id, raw_content) {
-            Ok(message) => {
-                let response = self.handle_request(message.content);
-                self.send_response(response);
+        queue.push_back(start);
+        visited.insert(start, true);
+
+        while let Some(current) = queue.pop_front() {
+            if current == goal {
+                // Reconstruct of the path
+                let mut path = vec![goal];
+                let mut node = goal;
+                while let Some(&p) = parent.get(&node) {
+                    path.push(p);
+                    node = p;
+                }
+                path.reverse();
+                return Some(path);
             }
-            Err(str) => panic!("{}", str),
+
+            if let Some(neighbors) = graph.get(&current) {
+                for &neighbor in neighbors {
+                    if !visited.contains_key(&neighbor) {
+                        queue.push_back(neighbor);
+                        visited.insert(neighbor, true);
+                        parent.insert(neighbor, current);
+                    }
+                }
+            }
+        }
+
+        None // No path found !BIG PROBLEM!
+    }
+
+  
+
+    // Remove a neibourg from a node in case of a crash
+    fn remove_neighbor(graph: &mut HashMap<NodeId, Vec<NodeId>>, node: NodeId, neighbor: NodeId) {
+        if let Some(neighbors) = graph.get_mut(&node) {
+            neighbors.retain(|&n| n != neighbor);
         }
     }
-
-    fn send_response(&mut self, _response: Self::ResponseType) {
-        // send response
+    
+    
+    /*
+    Start the flood protocol to fill up the hashmap and create the tree of the graph.
+    Small remainder the hash map is composed in this way HashMap<NodeId, Vec<NodeId>> , The NodeId 
+    and the list of it's neighbour 
+     */
+   
+    fn initialization(&mut self)
+    {
+                
     }
-
-    fn handle_request(&mut self, request: Self::RequestType) -> Self::ResponseType;
-
-    fn get_sever_type() -> ServerType;
+    
+    fn start(&mut self)
+    {
+        
+    }
+    
 }
 
-pub(crate) struct ChatServer;
 
-impl Server for ChatServer {
-    type RequestType = ChatRequest;
-    type ResponseType = ChatResponse;
 
-    fn handle_request(&mut self, request: Self::RequestType) -> Self::ResponseType {
-        match request {
-            ChatRequest::ClientList => {
-                println!("Sending ClientList");
-                ChatResponse::ClientList(vec![1, 2])
-            }
-            ChatRequest::Register(id) => {
-                println!("Registering {}", id);
-                ChatResponse::ClientList(vec![1, 2])
-            }
-            ChatRequest::SendMessage {
-                message,
-                to,
-                from: _,
-            } => {
-                println!("Sending message \"{}\" to {}", message, to);
-                // effectively forward message
-                ChatResponse::MessageSent
-            }
-        }
-    }
 
-    fn get_sever_type() -> ServerType {
-        ServerType::Chat
-    }
-}
 
 fn main() {
-    let mut server = ChatServer;
-    server.on_request_arrived(1, 1, ChatRequest::Register(1).stringify());
-    server.on_request_arrived(
-        1,
-        1,
-        ChatRequest::SendMessage {
-            from: 1,
-            to: 2,
-            message: "Hello".to_string(),
-        }
-            .stringify(),
-    );
-    server.on_request_arrived(1, 1, "ServerType".to_string());
+
 }
