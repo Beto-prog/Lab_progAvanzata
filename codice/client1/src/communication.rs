fb8429ff276755d19a9dbcd26d94d7ca9ad03e2b
Lorenzo Cortese <lorenzo.cortese@studenti.unitn.it>
2025-05-29 12:32:46 +0200

    fix

--- codice/client1/src/communication.rs
+++ codice/client1/src/communication.rs
@@ -25,7 +25,7 @@ impl Client1 {
                 cmd if cmd.starts_with("file?(") && cmd.ends_with(")") => {
                     if let Some(name) = cmd.strip_prefix("file?(").and_then(|s| s.strip_suffix(")")) {
                         if self.files_names.lock().expect("Failed to lock").contains(&name.parse::<String>().ok().expect("Failed to get files names")) {
-                            self.selected_file_name = name.to_string();
+                            //self.selected_file_name = name.to_string();
                             self.send_message(dest_id, cmd);
                             "CLIENT1: OK".to_string()
                         } else {
